11b9fe4218f00b2d87b42057721ba3b29b467f9a
Żan <konto564@wp.pl>
2022-05-30 21:02:16 +0200

    Add files via upload

---
+++ Delta/Delta/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Delta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int x1;
+            int x2;
+            int delta;
+
+            Console.Write("Podaj wartość parametru a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Podaj wartość parametru a: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Podaj wartość parametru a: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            delta = b ^ 2 - 4 * a * c; 
+
+            if(delta > 0)
+            {
+                x1 = (int)(-b - Math.Sqrt(delta)) / (2 * a);
+
+                x2 = (int)(-b + Math.Sqrt(delta)) / (2 * a);
+            }
+            else if(delta == 0)
+            {
+                x1 = (int)(-b / 2 * a);
+            }
+            else
+            {
+                Console.WriteLine("równanie nie ma pierwiastków rzeczywistych");
+                Console.ReadLine();
+            }
+        }
+    }
+}
